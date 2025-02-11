548f9de1296652dda79b7c87e40b6ad14fbaf667
M NAVYASREE <navyam481@gmail.com>
2025-02-11 15:05:02 +0530

    day9

---
+++ SBA-1/src/test/java/com/example/emo/Sba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
